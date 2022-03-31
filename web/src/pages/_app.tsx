91812a2ef574e831fa46e9c80571b9bfffbe67ee
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-03-31 19:57:22 -0300

    app corregido

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
-
 import "../styles/globals.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
