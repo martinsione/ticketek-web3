cc6544a5112f5e25e9d0320c0bb766cfc3aa098e
Diego Zamora <diegoz.estudio@gmail.com>
2022-03-31 19:30:16 -0300

    first card

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,22 @@
 import { Box } from "@chakra-ui/react";
 
+// import Card from "../components/Card/Card";
+
+// interface Evento {
+//   name: string;
+//   image: string
+// }
+
+// const prueba: Evento = {
+//   name: "Queen - Last Concert",
+//   image: "https://1.bp.blogspot.com/-7aLApwFsJPc/WC1svhmC3II/AAAAAAAAK-I/DeXeC4WsDc4fOQBdU1lFMQVf2R8dan7tACLcB/s1600/front.jpg"
+// }
+
 export default function Home() {
   return (
     <div>
       <Box>Landing Page</Box>
+      {/* <Card image={prueba.image} name={prueba.name} /> */}
     </div>
   );
 }
