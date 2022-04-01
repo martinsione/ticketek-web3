23d0a9fe1cd7aa99b8b04b0ec5a3af82f2c70460
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-04-01 10:25:28 -0300

    Revert "LogIn conflict solution"
    This reverts commit 90a866a935816528b7936ed678aef6fbc0027dc9.

---
+++ web/src/components/NavBar/LogIn.tsx
@@ -0,0 +1,69 @@
+import Swal from "sweetalert2";
+import { FaUser, FaUserCircle } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useWeb3React } from "@web3-react/core";
+import { IconButton, Button } from "@chakra-ui/react";
+
+import injected from "../Wallet/connector";
+
+function LogIn() {
+  const router = useRouter();
+  const { activate, deactivate, account } = useWeb3React();
+  const [loading, setLoading] = useState(false);
+
+  const [metamask, setMetamask] = useState(false);
+  const [setError] = useState<any>();
+
+  useEffect(() => {
+    setMetamask(window.ethereum && true);
+  }, []);
+
+  const handleConnect = () => {
+    setLoading(true);
+    // si no tiene metamask redireccionamos a que se instale
+    if (!metamask) router.push("https://metamask.io/download/");
+    else if (!account) {
+      // si no hay cuenta conectamos
+      try {
+        activate(injected).then(() => {
+          setLoading(false);
+          if (account) {
+            Swal.fire({
+              title: "Wallet connected",
+              icon: "success",
+            });
+          }
+        });
+      } catch (e: any) {
+        setError(e);
+      }
+    }
+    // si hay cuenta desconectamos
+    if (account) {
+      try {
+        deactivate();
+        setLoading(false);
+      } catch (e) {
+        setError(true);
+      }
+    }
+  };
+
+  return (
+    <Button
+      colorScheme="pink"
+      fontSize={25}
+      fontWeight={700}
+      h={50}
+      isLoading={loading}
+      variant="solid"
+      w={100}
+      onClick={handleConnect}
+    >
+      Login
+    </Button>
+  );
+}
+
+export default LogIn;
