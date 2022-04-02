750b65e2f900433ade9f0bf17cb4d7477d483566
Diego Zamora <diegoz.estudio@gmail.com>
2022-04-01 21:49:50 -0300

    primeros datos traidos del back

--- web/src/components/EventCardsViewer/EventCardsViewer.tsx
+++ web/src/components/EventCardsViewer/EventCardsViewer.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import Web3 from "web3";
+import React, { useState } from "react";
 import { Box, Text, Flex } from "@chakra-ui/react";
 
 import { fakeEvents } from "../fakeEvents";
+import { abi } from "../Ticket.json"
 import Card from "../Card/Card";
 
 interface Props {
@@ -10,10 +12,30 @@ interface Props {
   range: [number, number];
 }
 
+const adrress: string = '0xfdAaeeaD0c2b78E5B06a650D8271dE0A30CE24F2'
+
+const web = new Web3('https://ropsten.infura.io/v3/205cede8cdd24eec87b57ce48768889f')
+
+const contract = new web.eth.Contract(abi, adrress)
+
+// const arrContratos = contract.methods.getArr().call().then(res)
+// // aqui traeriamos el arreglo con todos los contratitos
+
+// const metadataArr = arrContratos.map(e => {
+//   const contractMap = new web.eth.Contract(abi, e)
+//   return contractMap.methods.getURI().call().then(res)
+// })
+// // este seria el arreglito que mapeariamos, con todos los datos de los eventos
+
 export default function EventCardViewer({ title, range }: Props) {
   const events = fakeEvents();
   // const events = fakeEvents().filter((ev) => ev.city === "Bogota");
   const [min, max] = range;
+  const [namecontrato, setNamecontrato] = useState("")
+  const [symbol, setSymbol] = useState("")
+
+  contract.methods.name().call().then(res => setNamecontrato(res))
+  contract.methods.symbol().call().then(res => setSymbol(res))
 
   return (
     <div>
@@ -27,6 +49,8 @@ export default function EventCardViewer({ title, range }: Props) {
           justify="space-around"
           wrap="wrap"
         >
+          <h1>{namecontrato}</h1>
+          <h1>{symbol}</h1>
           {events.map(
             (ev, ndx) =>
               ndx >= min &&
