bd594c4467c577eb9dc3fdf87b2016c481a08f6c
Alejo <alejoivanrojas@gmail.com>
2022-04-02 21:15:27 -0300

    layout fixed for real

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,8 +4,8 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import "../styles/globals.css";
+import Layout from "../components/estructura/Layout";
 import getLibrary from "../components/Wallet/library";
-import Layout from "../components/Layout/Layout";
 
 const theme = extendTheme({
   fonts: {
