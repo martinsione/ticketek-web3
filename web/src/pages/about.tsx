902955e11ac0cdf96408fb68471ced7d7668b3ed
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-04-03 22:48:15 -0300

    fixed navBar

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,9 +1,5 @@
-import {
-  IoAnalyticsSharp,
-  IoLogoBitcoin,
-  IoLockClosed,
-} from 'react-icons/io5';
-import { ReactElement } from 'react';
+import { IoAnalyticsSharp, IoLogoBitcoin, IoLockClosed } from "react-icons/io5";
+import { ReactElement } from "react";
 import {
   Container,
   SimpleGrid,
@@ -16,7 +12,8 @@ import {
   Icon,
   useColorModeValue,
   Box,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
+import NavBar from "../components/NavBar/NavBar";
 
 interface FeatureProps {
   text: string;
@@ -33,7 +30,8 @@ function Feature({ text, icon, iconBg }: FeatureProps) {
         h={8}
         justify="center"
         rounded="full"
-        w={8}>
+        w={8}
+      >
         {icon}
       </Flex>
       <Text fontWeight={600}>{text}</Text>
@@ -43,88 +41,91 @@ function Feature({ text, icon, iconBg }: FeatureProps) {
 
 export default function About() {
   return (
-    <Container maxW="5xl" py={12}>
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-        <Stack spacing={4}>
-          <Text
-            alignSelf="flex-start"
-            bg={useColorModeValue('blue.50', 'blue.900')}
-            color="blue.400"
-            fontSize="sm"
-            fontWeight={600}
-            p={2}
-            rounded="md"
-            textTransform="uppercase">
-            Our Story
-          </Text>
-          <Heading>A new way to buy and hodl your tickets</Heading>
-          <Text color="gray.500" fontSize="lg">
-          A Henry&apos;s team who wants to change the events industry
-          </Text>
-          <Stack
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue('gray.100', 'gray.700')}
+    <>
+      <NavBar long={false} />
+      <Container maxW="5xl" py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Text
+              alignSelf="flex-start"
+              bg={useColorModeValue("blue.50", "blue.900")}
+              color="blue.400"
+              fontSize="sm"
+              fontWeight={600}
+              p={2}
+              rounded="md"
+              textTransform="uppercase"
+            >
+              Our Story
+            </Text>
+            <Heading>A new way to buy and hodl your tickets</Heading>
+            <Text color="gray.500" fontSize="lg">
+              A Henry&apos;s team who wants to change the events industry
+            </Text>
+            <Stack
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
+              }
+              spacing={4}
+            >
+              <Feature
+                icon={
+                  <Icon as={IoAnalyticsSharp} color="yellow.500" h={5} w={5} />
+                }
+                iconBg={useColorModeValue("yellow.100", "yellow.900")}
+                text="Incomes directly linked to the artists."
               />
-            }
-            spacing={4}>
-            <Feature
-              icon={
-                <Icon as={IoAnalyticsSharp} color="yellow.500" h={5} w={5} />
-              }
-              iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-              text="Incomes directly linked to the artists."
-            />
-            <Feature
-              icon={<Icon as={IoLogoBitcoin} color="green.500" h={5} w={5} />}
-              iconBg={useColorModeValue('green.100', 'green.900')}
-              text="Buy now and hold for ever"
-            />
-            <Feature
-              icon={
-                <Icon as={IoLockClosed} color="purple.500" h={5} w={5} />
-              }
-              iconBg={useColorModeValue('purple.100', 'purple.900')}
-              text="Segurity during all the process"
-            />
+              <Feature
+                icon={<Icon as={IoLogoBitcoin} color="green.500" h={5} w={5} />}
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text="Buy now and hold for ever"
+              />
+              <Feature
+                icon={<Icon as={IoLockClosed} color="purple.500" h={5} w={5} />}
+                iconBg={useColorModeValue("purple.100", "purple.900")}
+                text="Segurity during all the process"
+              />
+            </Stack>
           </Stack>
-        </Stack>
-        <Flex 
-        margin={70}>
-        <Box
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 30,
-            left: 0,
-            backgroundImage: `url(${"https://images.pexels.com/photos/3052360/pexels-photo-3052360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}
-          cursor="pointer"
-          height="300px"
-          mt={-12}
-          pos="relative"
-          rounded="lg"
-          width="350px">
-          <Image
-            height={300}
-            objectFit="cover"
-            rounded="lg"
-            src="https://images.pexels.com/photos/3052360/pexels-photo-3052360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            width={330}
-          />
-        </Box>
-        </Flex>
-      </SimpleGrid>
-    </Container>
+          <Flex margin={70}>
+            <Box
+              _after={{
+                transition: "all .3s ease",
+                content: '""',
+                w: "full",
+                h: "full",
+                pos: "absolute",
+                top: 30,
+                left: 0,
+                backgroundImage: `url(${"https://images.pexels.com/photos/3052360/pexels-photo-3052360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"})`,
+                filter: "blur(15px)",
+                zIndex: -1,
+              }}
+              _groupHover={{
+                _after: {
+                  filter: "blur(20px)",
+                },
+              }}
+              cursor="pointer"
+              height="300px"
+              mt={-12}
+              pos="relative"
+              rounded="lg"
+              width="350px"
+            >
+              <Image
+                height={300}
+                objectFit="cover"
+                rounded="lg"
+                src="https://images.pexels.com/photos/3052360/pexels-photo-3052360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                width={330}
+              />
+            </Box>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </>
   );
 }
--- web/src/pages/help.tsx
+++ web/src/pages/help.tsx
@@ -1,48 +1,55 @@
-
-import {
-  Box,
-  Heading,
-  Container,
-  Text,
-  Stack,
-  Link
-} from '@chakra-ui/react';
+import { Box, Heading, Container, Text, Stack, Link } from "@chakra-ui/react";
+import NavBar from "../components/NavBar/NavBar";
 
 export default function Help() {
   return (
-    <Container maxW="3xl">
+    <>
+      <NavBar long={false} />
+      <Container maxW="3xl">
         <Stack
           as={Box}
           py={{ base: 20, md: 36 }}
           spacing={{ base: 8, md: 14 }}
-          textAlign="center">
+          textAlign="center"
+        >
           <Heading
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
             fontWeight={600}
-            lineHeight="110%">
+            lineHeight="110%"
+          >
             What is metamask? <br />
             <Text as="span" color="green.300">
               Quick start
             </Text>
           </Heading>
           <Text color="gray.500">
-          MetaMask provides the simplest yet most secure way to connect to blockchain-based applications. You are always in control when interacting on the new decentralized web.  
+            MetaMask provides the simplest yet most secure way to connect to
+            blockchain-based applications. You are always in control when
+            interacting on the new decentralized web.
           </Text>
           <Stack
             align="center"
             alignSelf="center"
             direction="column"
             position="relative"
-            spacing={3}>
-          <iframe allowFullScreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" frameBorder="0" height="315" src="https://www.youtube.com/embed/YVgfHZMFFFQ" title="YouTube video player" width="560" />
+            spacing={3}
+          >
+            <iframe
+              allowFullScreen
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              frameBorder="0"
+              height="315"
+              src="https://www.youtube.com/embed/YVgfHZMFFFQ"
+              title="YouTube video player"
+              width="560"
+            />
 
-             <Link  color='teal.500' href="https://metamask.io/">
-               Learn More
-             </Link>
-            
+            <Link color="teal.500" href="https://metamask.io/">
+              Learn More
+            </Link>
           </Stack>
         </Stack>
       </Container>
+    </>
   );
 }
-
