3e8140bc92ac8cd66b24ca0a6a0dd8856f4a6004
Franco Angulo <francoangulo2001@gmail.com>
2022-04-07 15:39:49 -0300

    Added some event rotues

--- web/src/pages/api/events/index.tsx
+++ web/src/pages/api/events/index.tsx
@@ -1,7 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import data from "../../../components/fakeEvent.json";
+import prisma from "../../../lib/prisma";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(data);
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const { address, city, name, symbol } = req.body;
+    try {
+        if (req.method === "POST") {
+            const response = await prisma.contract.create({
+                data: {
+                    address,
+                    city,
+                    name,
+                    symbol,
+                },
+            });
+            return res.status(200).json(response);
+        }
+        const response = await prisma.contract.findMany();
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
 }
