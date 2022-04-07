a342dfbf08e2317bf6ad768ee9ff366adaef16d8
Martin Sione <martinsione03@gmail.com>
2022-04-07 16:23:25 -0300

    Add mock data for prisma

--- web/src/pages/api/users/[user].tsx
+++ web/src/pages/api/users/[user].tsx
@@ -1,35 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+
 import prisma from "../../../lib/prisma";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    // Obtengo el parámetro que viene por la query, siempre va a se la walletAddress
-    const user = req.query.user.toString();
-    //     Método DELETE
-    if (req.method === "DELETE") {
-        console.log(user);
-        const response = await prisma.user.delete({
-            where: { walletAddress: user },
-        });
+  // Obtengo el parámetro que viene por la query, siempre va a se la walletAddress
+  const user = req.query.user.toString();
+  //     Método DELETE
+  if (req.method === "DELETE") {
+    console.log(user);
+    const response = await prisma.user.delete({
+      where: { walletAddress: user },
+    });
 
-        return res.status(200).json(response);
-        //   Método PUT
-    } else if (req.method === "PUT") {
-        const { name, email, image } = req.body;
-        //   Falta corroborar si no se envia nombre, etc etc
-        const response = await prisma.user.update({
-            where: {
-                walletAddress: user,
-            },
-            data: { name, email, image },
-        });
-        return res.status(200).json(response);
-    }
-    //     Método GET
-    const response = await prisma.user.findUnique({
-        where: { walletAddress: user },
+    return res.status(200).json(response);
+    //   Método PUT
+  }
+  if (req.method === "PUT") {
+    const { name, email, image } = req.body;
+    //   Falta corroborar si no se envia nombre, etc etc
+    const response = await prisma.user.update({
+      where: {
+        walletAddress: user,
+      },
+      data: { name, email, image },
     });
     return res.status(200).json(response);
+  }
+  //     Método GET
+  const response = await prisma.user.findUnique({
+    where: { walletAddress: user },
+  });
+  return res.status(200).json(response);
 }
---
+++ web/src/prisma/data.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/prefer-default-export
+export const users = [
+  {
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    walletAddress: "0xdeF0d235554B827c07d06f09Ac1b28BE357C5814",
+  },
+  {
+    name: "Juan Perez",
+    email: "juanperez@hotmail.com",
+    walletAddress: "0x780b74BA70071cc89a54caA0D8F86f881602A942",
+  },
+  {
+    name: "Pepito",
+    email: "pepito@gmail.com",
+    walletAddress: "0x2e95258d66B39c2789dB3Ea16437C427F3277c88",
+  },
+  {
+    name: "Maria",
+    email: "maria@outlook.com",
+    walletAddress: "0x2e84e8833aB3aBD36a0B614a4aEEA3218faB10C8",
+  },
+];
