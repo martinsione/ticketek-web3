9cee7e9f2751036d94c9e6bd398df1bf4b3e5f8a
Andrés Vilá <vilaandres@gmail.com>
2022-04-09 13:11:32 -0500

    Terminando CardSlider

--- web/src/prisma/data.js
+++ web/src/prisma/data.js
@@ -1,43 +1,127 @@
 // eslint-disable-next-line import/prefer-default-export
 module.exports = {
-    users: [
-        {
-            name: "John Doe",
-            email: "johndoe@gmail.com",
-            walletAddress: "0xdeF0d235554B827c07d06f09Ac1b28BE357C5814",
-            image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
-        },
-        {
-            name: "Juan Perez",
-            email: "juanperez@hotmail.com",
-            walletAddress: "0x780b74BA70071cc89a54caA0D8F86f881602A942",
-            image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
-        },
-        {
-            name: "Pepito",
-            email: "pepito@gmail.com",
-            walletAddress: "0x2e95258d66B39c2789dB3Ea16437C427F3277c88",
-            image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
-        },
-        {
-            name: "Maria",
-            email: "maria@outlook.com",
-            walletAddress: "0x2e84e8833aB3aBD36a0B614a4aEEA3218faB10C8",
-            image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
-        },
-    ],
-    contracts: [
-        {
-            address: "0x945eD39416121076ADB07c493e306b6D9E541b09",
-            city: "Mendoza",
-            name: "Queen",
-            symbol: "QNN",
-        },
-        {
-            address: "0x141F9b8b27F8488DCe661aB850BEc72439E80f8A",
-            city: "Medellín",
-            name: "Green Day",
-            symbol: "GND",
-        },
-    ],
+  users: [
+    {
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      walletAddress: "0xdeF0d235554B827c07d06f09Ac1b28BE357C5814",
+      image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+    },
+    {
+      name: "Juan Perez",
+      email: "juanperez@hotmail.com",
+      walletAddress: "0x780b74BA70071cc89a54caA0D8F86f881602A942",
+      image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+    },
+    {
+      name: "Pepito",
+      email: "pepito@gmail.com",
+      walletAddress: "0x2e95258d66B39c2789dB3Ea16437C427F3277c88",
+      image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+    },
+    {
+      name: "Maria",
+      email: "maria@outlook.com",
+      walletAddress: "0x2e84e8833aB3aBD36a0B614a4aEEA3218faB10C8",
+      image: "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+    },
+  ],
+  contracts: [
+    {
+      address: "0x945eD39416121076ADB07c493e106b6D9E541b09",
+      city: "Mendoza",
+      name: "Queen",
+      symbol: "QNN",
+    },
+    {
+      address: "0x141F9b8b27F8488DCg661aB850BEc72439E80f8A",
+      city: "Medellín",
+      name: "Green Day",
+      symbol: "GND",
+    },
+    {
+      address: "0x945eD39416121076SRB07c493e306b1D9E541b09",
+      city: "Bogotá",
+      name: "Tamal",
+      symbol: "TML",
+    },
+    {
+      address: "0x141F9b8b27F8488DCe661aB840BEc72439E80f8A",
+      city: "Buenos Aires",
+      name: "Gary Numan",
+      symbol: "GNM",
+    },
+    {
+      address: "0x945eD39416121076EQB07c493e306b6D9E541b09",
+      city: "Mendoza",
+      name: "Metallica",
+      symbol: "MTL",
+    },
+    {
+      address: "1x141F9b8b27F8488DWe661aB850BEc72439E80f8A",
+      city: "Medellín",
+      name: "Sex Pistols",
+      symbol: "SEX",
+    },
+    {
+      address: "0x945eD39416121071SRB07c493e306b6D9E541b09",
+      city: "Bogotá",
+      name: "Raime",
+      symbol: "RAM",
+    },
+    {
+      address: "0x141F9b8b27F8482DCe661aB850BEc72430E80f8A",
+      city: "Buenos Aires",
+      name: "Ramones",
+      symbol: "RMN",
+    },
+    {
+      address: "0x945eD39412121276ADB07c493e306b6D9E541b09",
+      city: "Mendoza",
+      name: "Caifanes",
+      symbol: "CFN",
+    },
+    {
+      address: "0x141F9b8b27F8488DCe661aB650BEc72439E80f8A",
+      city: "Medellín",
+      name: "Arca",
+      symbol: "ARC",
+    },
+    {
+      address: "0x945eD39416121076SRB07c491r306b6D9E541b09",
+      city: "Bogotá",
+      name: "Katy Perry",
+      symbol: "KPY",
+    },
+    {
+      address: "0x141F9b8b27W8488DCw661aB850BEc72439E80f8A",
+      city: "Buenos Aires",
+      name: "Pet Shop Boys",
+      symbol: "PSB",
+    },
+    {
+      address: "0x945eD39416131076ADB07c493e306r6D9E541b09",
+      city: "Mendoza",
+      name: "Los Punsetes",
+      symbol: "PUN",
+    },
+    {
+      address: "0x141F9b8b27F8488DCq611aB850BEc72439E80f8A",
+      city: "Medellín",
+      name: "Kraken",
+      symbol: "KRK",
+    },
+    {
+      address: "0x945eD39416121036SXB07c493e306b6D9E541b09",
+      city: "Bogotá",
+      name: "Carcass",
+      symbol: "CRC",
+    },
+    {
+      address: "0x141F9b8b27F8488DCe661aT850WEc72439E80f8A",
+      city: "Buenos Aires",
+      name: "Wilfrido Vargas",
+      symbol: "WFR",
+    },
+  ],
 };
