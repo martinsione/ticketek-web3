af1f7fcc14ad21768f52cec7d78aef83dc44ad66
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-04-11 19:37:05 -0300

    unused components taken off import

--- web/src/components/scripts/algoliaIndexUpdater.js
+++ web/src/components/scripts/algoliaIndexUpdater.js
@@ -14,6 +14,6 @@ const eventsWithID = events.map((event) => ({ ...event, objectID: event.id }));
 index
   .saveObjects(eventsWithID)
   .then((objectID) => {
-    console.log(objectID);
+    //console.log(objectID);
   })
-  .catch((error) => console.log(error));
+  .catch((error) => null);
