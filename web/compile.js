e427fff9bd88bdecd06ec4a032a3a044dfc977dd
LNFrettes <ln.frettes@gmail.com>
2022-04-13 12:11:06 -0300

    add of userComands

---
+++ web/compile.js
@@ -0,0 +1,8 @@
+// const path = require("path");
+// const fse = require("fs-extra");
+// const solc = require("solc");
+
+// const lotteryPath = path.resolve(__dirname, "contracts", "Price.sol");
+// const source = fse.readFileSync(lotteryPath, "utf8");
+
+// module.exports = solc.compile(source, 1).contracts[":Price"];
