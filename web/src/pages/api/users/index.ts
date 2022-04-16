5b27b8c48d3854883ee7bd9db0a803f0e8ec380a
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-04-16 12:00:37 -0300

    last fix

--- web/src/pages/api/users/index.ts
+++ web/src/pages/api/users/index.ts
@@ -4,7 +4,6 @@ import prisma from "../../../lib/prisma";
 
 const WAValidator = require("wallet-address-validator");
 
-import { verify } from "jsonwebtoken";
 import Auth from "../../../lib/auth";
 
 export default async function handler(
@@ -46,32 +45,6 @@ export default async function handler(
           }
         }
       );
-      //   verify(JWT, process.env.SECRET_WORD as string, async (error, user) => {
-      //     if (error) {
-      //       return res.status(403).json({ error: "unauthorized" });
-      //     }
-      //     const valid = WAValidator.validate(walletAddress, "ETH");
-      //     if (valid) {
-      //       // Se verifica que el input de email sea válido
-      //       if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-      //         const response: void = await prisma.user
-      //           .create({
-      //             data: {
-      //               name,
-      //               email,
-      //               walletAddress,
-      //             },
-      //           })
-      //           .then(() => {
-      //             res.status(200).json({ message: "email changed" });
-      //           })
-      //           .catch(() => {
-      //             res.status(400).json({ message: "not a valid email" });
-      //           });
-      //       }
-      //     }
-      //   });
-      //   // Se verifica que el input de walletAddress sea válido
     }
     // if (req.method === "DELETE") {         <----  comentado hasta saber que hacer
     //   const { cookies } = req;
